efd74af2cb8d464305ee4fd1ad50deecfa7a0e43
evyatar shlar <evyatar.szlar@gmail.com>
2026-01-20 17:20:03 +0200

    chore: clean up empty code change sections in the changes log

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ if (process.env.NODE_ENV === 'production') {
             'http://localhost:5173',
             'http://localhost:5174',
             'http://127.0.0.1:5174',
+            'http://localhost:5175',
+            'http://127.0.0.1:5175',
         ],
         credentials: true
     }
